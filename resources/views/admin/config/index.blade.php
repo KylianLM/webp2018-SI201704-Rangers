ff4def2ec8c39e67822ee31c58128e67052be7af
Kylian LE METTE <kylian.le.mette@gmail.com>
2017-04-18 16:38:49 +0200

    [CHG] Config View

--- resources/views/admin/config/index.blade.php
+++ resources/views/admin/config/index.blade.php
@@ -1,13 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>YOLO</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Configuration</div>
+
+                <div class="panel-body">
+                    yolo
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+@endsection
