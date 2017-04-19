5b9253fd1afd528006b3084525da81cc098b62cb
Kylian LE METTE <kylian.le.mette@gmail.com>
2017-04-19 13:36:08 +0200

    [ADD] Message Routing
    Delete Create method

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -27,17 +27,7 @@ class MessageController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return view('admin.message.index');
     }
 
     /**
