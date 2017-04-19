3b12cef529e562a7218bbefad2b40a249f092f69
Kylian LE METTE <kylian.le.mette@gmail.com>
2017-04-19 14:32:29 +0200

    [CHG] Template

---
+++ resources/views/admin/message/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Messages
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+
+        </section>
+        <!-- /.content -->
+    </div>
+
+@endsection
